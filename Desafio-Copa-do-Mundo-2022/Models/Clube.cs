a31f0b0008d8ea11845f7844718aef54e65955d6
Rafael Seiffer Melazzo <rafamelazzo@gmail.com>
2022-10-16 08:45:33 -0300

    Teste

--- Desafio-Copa-do-Mundo-2022/Models/Clube.cs
+++ Desafio-Copa-do-Mundo-2022/Models/Clube.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace API_Copa_do_Mundo_2022.Models // teste
+namespace API_Copa_do_Mundo_2022.Models
 {
     [Table("clubes")]
     public class Clube
