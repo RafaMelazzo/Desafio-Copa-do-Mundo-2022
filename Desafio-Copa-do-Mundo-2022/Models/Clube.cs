0e71c23205c80d5465b9d7af9422f7d6baa0fbc3
Augusto Moreira <gustoux@gmail.com>
2022-10-15 22:47:29 -0300

    teste

--- Desafio-Copa-do-Mundo-2022/Models/Clube.cs
+++ Desafio-Copa-do-Mundo-2022/Models/Clube.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace API_Copa_do_Mundo_2022.Models
+namespace API_Copa_do_Mundo_2022.Models // teste
 {
     [Table("clubes")]
     public class Clube
